c95f39d67361674a6d98fc38f948f6dce4141e77
Chayzerkir <kiritokadzuto20@gmail.com>
2025-09-30 08:37:28 +0300

    1.1.2

--- Модуль 1/1.1/1.1.2/1.1.2/Program.cs
+++ Модуль 1/1.1/1.1.2/1.1.2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите три числа через enter для нахождения среднего арифметического:");
+        double n1 = Convert.ToInt32(Console.ReadLine());
+        double n2 = Convert.ToInt32(Console.ReadLine());
+        double n3 = Convert.ToInt32(Console.ReadLine());
+        double MidArif = (n1+n2+n3)/3;
+        Console.WriteLine($"Среднее арифметическое: ({n1}+{n2}+{n3})/3 = {MidArif}");
+    }
+}
