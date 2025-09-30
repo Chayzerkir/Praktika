3ff7a997f29353d37f8b8a2c5902c66b30cefb36
Chayzerkir <kiritokadzuto20@gmail.com>
2025-09-30 10:29:01 +0300

    1.1.4

---
+++ Модуль 1/1.1/1.1.4/1.1.4/1.1.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10];
+        Random rand = new Random();
+        Console.Write("Массив из 10 случайных чисел: ");
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(0, 100);
+            Console.Write(array[i] + " ");
+        }
+        for (int i = 0;i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+        Console.WriteLine($"\nСумма всех элементов = {sum}");
+    }
+}
