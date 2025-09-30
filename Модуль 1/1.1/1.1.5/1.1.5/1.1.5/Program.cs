372d7c9192a0c92a71e6e5c12fd4221b4ad6ceec
Chayzerkir <kiritokadzuto20@gmail.com>
2025-09-30 10:47:57 +0300

    1.1.5

---
+++ Модуль 1/1.1/1.1.5/1.1.5/1.1.5/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int[] array = new int[20];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(0, 100);
+        }
+        for (int i = 0;i < array.Length; i++)
+        {
+            if (array[i] % 3 == 0 && array[i] % 5 == 0)
+            {
+                Console.WriteLine($"FizzBuzz({array[i]})");
+            }
+            else if (array[i] % 3 == 0)
+            {
+                Console.WriteLine($"Fizz({array[i]})");
+            }
+            else if (array[i] % 5 == 0)
+            {
+                Console.WriteLine($"Buzz({array[i]})");
+            }
+            else
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
