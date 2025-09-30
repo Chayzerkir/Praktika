58078223a0e8c49b18df38c66ab6e7dd8c4882ad
Chayzerkir <kiritokadzuto20@gmail.com>
2025-09-30 09:43:00 +0300

    1.1.3

--- Модуль 1/1.1/1.1.3/1.1.3/1.1.3/Program.cs
+++ Модуль 1/1.1/1.1.3/1.1.3/1.1.3/Program.cs
@@ -2,6 +2,18 @@
 {
     static void Main()
     {
-
+        Console.WriteLine("Напишите текст, в котором будем искать подстроку: ");
+        string text = Console.ReadLine();
+        Console.WriteLine("Напишите подстроку");
+        string subtext = Console.ReadLine();
+        bool contain = text.Contains(subtext);
+        if (contain)
+        {
+            Console.WriteLine("Подстрока содержится в тексте.");
+        }
+        else
+        {
+            Console.WriteLine("Подстрока не содержится в тексте.");
+        }
     }
 }
