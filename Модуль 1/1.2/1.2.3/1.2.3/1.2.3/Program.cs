82b8f41d968f8f54a9feb5e6803914cd11797629
Chayzerkir <kiritokadzuto20@gmail.com>
2025-09-30 22:22:54 +0300

    1.2.3

---
+++ Модуль 1/1.2/1.2.3/1.2.3/1.2.3/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите, сколько простых чисел необходимо вывести на экран: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int SimpleNum = 0;
+        int WriteNum = 0;
+        while (WriteNum < N)
+        {
+            SimpleNum++;
+            if (IsSimple(SimpleNum))
+            {
+                if (WriteNum % 10 == 0)
+                {
+                    Console.Write($"\n{SimpleNum} ");
+                }
+                else
+                {
+                    Console.Write($"{SimpleNum} ");
+                }
+            WriteNum++;
+            }
+        }
+    }
+
+    static bool IsSimple(int N)
+    {
+        if (N < 2) return false;
+        for (int i = 2; i <= (int)(N / 2); i++)
+        {
+            if (N % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
