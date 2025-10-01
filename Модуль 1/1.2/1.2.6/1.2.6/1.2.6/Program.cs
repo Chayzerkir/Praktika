fcd8349f6e4af1c2fbd9a872386869ce5a56a0eb
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-01 10:29:05 +0300

    1.2.6

---
+++ Модуль 1/1.2/1.2.6/1.2.6/1.2.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        double[] arr = new double[10];
+        double[] newArr = new double[10];
+        double min = -10.0;
+        double max = 10.0;
+
+        Console.WriteLine("Массив из 10-ти вещественных чисел: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = min + (random.NextDouble() * (max - min));
+            Console.Write($"\n{i+1}. {arr[i]:F4}");
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            double NumMin = arr.Min();
+            int IndNimMin = Array.IndexOf(arr, NumMin);
+            newArr[i] = IndNimMin;
+            arr[IndNimMin] = double.MaxValue;
+        }
+
+        Console.WriteLine("\n\nИндексы по возрастанию значений элементов массива: ");
+        for (int i = 0; i < newArr.Length; i++)
+        {
+            Console.Write($"\n{i + 1}. {newArr[i]+1}");
+        }
+    }
+}
