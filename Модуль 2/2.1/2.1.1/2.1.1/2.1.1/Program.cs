58906cf3a24beda7afb2935f3a30fe5c9522142e
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-02 09:49:42 +0300

    2.1.1

---
+++ Модуль 2/2.1/2.1.1/2.1.1/2.1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+Person person = new Person();
+
+person.write();
+person.print();
+
+class Person
+{
+    public int age;
+    public string name;
+    public string adress;
+
+
+    public void print()
+    {
+        Console.WriteLine($"Имя: {name};  Возраст: {age}; Адрес: {adress}");
+    }
+    public void write()
+    {
+        Console.WriteLine("Введите данные о человеке:");
+        Console.Write("Имя: ");
+        name = Console.ReadLine();
+
+        Console.Write("\nВозраст: ");
+        age = int.Parse(Console.ReadLine());
+
+        Console.Write("\nАдрес: ");
+        adress = Console.ReadLine();
+    }
+}
