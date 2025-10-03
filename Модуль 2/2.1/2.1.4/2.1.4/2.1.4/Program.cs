c38fd90a177a0bfd8751d1c734c7a1b8f253f9d9
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-03 10:41:11 +0300

    2.1.4

---
+++ Модуль 2/2.1/2.1.4/2.1.4/2.1.4/Program.cs
@@ -0,0 +1,49 @@
+﻿interface IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Фигура для вывода информации не определена.");
+    }
+}
+
+class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуется круг.");
+    }
+}
+
+class Rectangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуется прямоугольник.");
+    }
+}
+
+class Triangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисуется треугольник.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IDrawable[] drawable = new IDrawable[]
+        {
+            new Circle(),
+            new Rectangle(),
+            new Triangle()
+        };
+
+        foreach (IDrawable drawab in drawable)
+        {
+            drawab.Draw();
+        }
+    }
+}
