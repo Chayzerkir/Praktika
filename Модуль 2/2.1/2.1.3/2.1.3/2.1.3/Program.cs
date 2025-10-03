8691d66179c02ba22ba6cc93951f9c363c3cdb77
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-03 09:05:06 +0300

    2.1.3

---
+++ Модуль 2/2.1/2.1.3/2.1.3/2.1.3/Program.cs
@@ -0,0 +1,37 @@
+﻿class Author
+{
+    public string name;
+    public int dateofbirth;
+}
+
+class Book
+{
+    public string title;
+    public int year;
+    public Author author;
+    public Book(string title, int year, string name)
+    {
+        this.title = title;
+        this.year = year;
+
+        author = new Author();
+        author.name = name;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Книга '{title}' была создана в {year} году.\nСоздал данное произведение сам {author.name}.\n");
+    }
+}
+
+class Program()
+{
+    static void Main()
+    {
+        Book firstBook = new Book("Крутое название", 2025, "Кухаренко Егор");
+        Book secondBook = new Book("Хз не придумал", 1053, "Не Кухаренко");
+
+        firstBook.Print();
+        secondBook.Print();
+    }
+}
