444b86602a6e22ef4e4daaa61a37c9f250f9ae1c
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-08 09:13:15 +0300

    3.1.1

---
+++ Модуль 3/3.1.1/3.1.1/3.1.1/Program.cs
@@ -0,0 +1,46 @@
+﻿delegate int FindS();
+
+class Figure
+{
+    public int S;
+    public static int Soffigure(int operation)
+    {
+        switch (operation)
+        {
+            case 1: return 5 * 8;
+            case 2: return Convert.ToInt32(Math.PI * 5 * 5);
+            case 3: return Convert.ToInt32((8 * 5) / 2);
+        }
+        return 0;
+    }
+}
+
+class Rectangle: Figure
+{
+    public static int Print() => Figure.Soffigure(1);
+}
+
+class Circle: Figure
+{
+    public static int Print() => Figure.Soffigure(2);
+}
+
+class Triangle:  Figure
+{
+    public static int Print() => Figure.Soffigure(3);
+}
+
+class Program
+{
+    static void Main()
+    {
+        FindS rectangle = Rectangle.Print;
+        FindS circle = Circle.Print;
+        FindS triangle = Triangle.Print;
+
+        Console.WriteLine("Нахождение площади фигур:");
+        Console.WriteLine($"Прямоугольника: {rectangle()}");
+        Console.WriteLine($"Круга: {circle()}");
+        Console.WriteLine($"Треугольника: {triangle()}");
+    }
+}
