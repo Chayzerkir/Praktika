76f0c38c1f73c71cf60c32e2b42f3f5d59c4297f
Chayzerkir <kiritokadzuto20@gmail.com>
2025-10-10 09:25:17 +0300

    4.1.2

---
+++ Модуль 4/4.1.2/4.1.2/4.1.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface IProduct
+{
+    double GetCost();  
+    int GetStock();   
+}
+
+class Food : IProduct
+{
+    public string Name;
+    public double Price;
+    public int Quantity;
+
+    public Food(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public double GetCost() => Price * Quantity;
+    public int GetStock() => Quantity;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Food chereshnia = new Food("Черешня", 2, 50);
+        Food dinya = new Food("Дыня", 5.5, 20);
+
+        Console.WriteLine($"Товар: {chereshnia.Name}, Остаток: {chereshnia.GetStock()}, Общая стоимость: {chereshnia.GetCost()}");
+        Console.WriteLine($"Товар: {dinya.Name}, Остаток: {dinya.GetStock()}, Общая стоимость: {dinya.GetCost()}");
+    }
+}
